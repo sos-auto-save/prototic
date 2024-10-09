a80d80d4d37122d99137098ecc87cb37e675bc49
osmarmcn <99884631+osmarmcn@users.noreply.github.com>
2024-10-08 21:37:35 -0300

    ajuste tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 
 import './App.css'
+import './index.css'
 
 function App() {
  
 
   return (
     <>
-     
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </>
   )
 }
